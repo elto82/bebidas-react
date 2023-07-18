ce63b332c0c5907d276c1c010741d3de5e06e25b
Argiro <elto.82@gmai.com>
2023-07-18 09:23:51 -0500

    modal

--- src/components/Receta.jsx
+++ src/components/Receta.jsx
@@ -1,8 +1,43 @@
-import React, { useContext } from "react";
+import { useContext, useState } from "react";
 import { ModalContex } from "../context/ModalContex";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 450,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const Receta = ({ receta }) => {
-  const { setIdReceta } = useContext(ModalContex);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const { informacion, setIdReceta } = useContext(ModalContex);
+  //console.log(informacion);
+
+  const mostrarIngredientes = (informacion) => {
+    let ingredientes = [];
+    for (let i = 1; i < 16; i++) {
+      if (informacion[`strIngredient${i}`]) {
+        ingredientes.push(
+          <li key={`ingrediente-${i}`}>
+            {informacion[`strMeasure${i}`]}
+            {informacion[`strIngredient${i}`]}
+          </li>
+        );
+      }
+    }
+    return ingredientes;
+  };
 
   return (
     <div className="col-md-4 mb-3 text-center mt-4  ">
@@ -17,10 +52,54 @@ const Receta = ({ receta }) => {
           <button
             className="btn btn-block btn-primary"
             type="button"
-            onClick={() => setIdReceta(receta.idDrink)}
+            onClick={() => {
+              setIdReceta(receta.idDrink);
+              handleOpen();
+            }}
           >
             Ver Receta
           </button>
+          <Modal
+            open={open}
+            onClose={() => {
+              //setReceta(null);
+              setIdReceta(null);
+              handleClose();
+            }}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                {informacion.strDrink}
+                <br />
+                información:
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                {informacion.strInstructions}
+                <img
+                  className="img-fluid my-4"
+                  src={informacion.strDrinkThumb}
+                  alt={informacion.strDrink}
+                  style={{
+                    borderRadius: "10px",
+                    boxShadow: "0px 0px 10px rgba(0,0,0,0.5)",
+                    objectFit: "cover",
+                    objectPosition: "center",
+                    marginTop: "4px",
+                    marginBottom: "4px",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    width: "50%",
+                    height: "50%",
+                    display: "block",
+                  }}
+                />
+              </Typography>
+              <div>Ingredientes y Cantidades</div>
+              <ul>{mostrarIngredientes(informacion)}</ul>
+            </Box>
+          </Modal>
         </div>
       </div>
     </div>
--- src/context/ModalContex.jsx
+++ src/context/ModalContex.jsx
@@ -4,12 +4,26 @@ import { createContext, useEffect, useState } from "react";
 export const ModalContex = createContext();
 
 const ModalProvider = ({ children }) => {
+  const [informacion, setReceta] = useState({});
+
   const [idReceta, setIdReceta] = useState(null);
+
+  useEffect(() => {
+    const obtenerRecetaApi = async () => {
+      if (!idReceta) return;
+      const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idReceta}`;
+      const res = await axios(url);
+      setReceta(res.data.drinks[0]);
+    };
+    obtenerRecetaApi();
+  }, [idReceta]);
 
   return (
     <ModalContex.Provider
       value={{
+        informacion,
         setIdReceta,
+        //setReceta,
       }}
     >
       {children}
